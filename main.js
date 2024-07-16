f99933a8243715ca1ae0b10696540b75678334f3
Mawis <dev@m4w1s.com>
2024-07-17 01:57:30 +0300

    fix stats

--- main.js
+++ main.js
@@ -114,6 +114,7 @@ async function filterUsedWallets(wallets) {
 
       if (ownedNftCount) {
         wallets.splice(i, 1);
+        stats.alreadyOwned++;
       }
     } catch (e) {
       // Игнорируем
